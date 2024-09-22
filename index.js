85aed7d594d2b2894ad96f9d84f2929f4edb385d
Yan-7 <yanivalmagor7@gmail.com>
2024-09-22 11:22:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express  = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true}))
+
+app.get('/tacos', (req,res) => {
+    res.send('GET /tacos response')
+})
+
+app.post('/tacos', (req,res) => {
+    res.send('this is a post response')
+})
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+    
+})
